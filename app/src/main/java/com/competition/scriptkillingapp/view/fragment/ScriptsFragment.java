4d1100310db84052ea91d9f29b4d11985b72393d
JackYuCN <JackYuCN0224@gmail.com>
2023-04-04 15:03:23 +0800

    finish the basic layout of scripts fragment

--- app/src/main/java/com/competition/scriptkillingapp/view/fragment/ScriptsFragment.java
+++ app/src/main/java/com/competition/scriptkillingapp/view/fragment/ScriptsFragment.java
@@ -4,20 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.competition.scriptkillingapp.R;
+import com.competition.scriptkillingapp.adapter.ScriptRecViewAdapter;
+import com.competition.scriptkillingapp.model.Script;
+
+import java.util.ArrayList;
 
 public class ScriptsFragment extends Fragment {
+
     private View view;
+
+    private RecyclerView scriptsRecView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_scripts, container, false);
+
+        scriptsRecView = view.findViewById(R.id.hot_scripts_recommend_recview);
+
+        ArrayList<Script> scripts = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            scripts.add(new Script("测试样例" + i));
+        }
+
+        ScriptRecViewAdapter adapter = new ScriptRecViewAdapter(view.getContext());
+        adapter.setScripts(scripts);
+
+        scriptsRecView.setAdapter(adapter);
+        scriptsRecView.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
         return view;
     }
