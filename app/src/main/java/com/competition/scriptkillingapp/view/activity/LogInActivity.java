f31458fe8233d15d512dab3cb086d6d8c24a1f69
JackYuCN <JackYuCN0224@gmail.com>
2023-04-10 15:24:12 +0800

    Add the basic layout of My Fragment

--- app/src/main/java/com/competition/scriptkillingapp/view/activity/LogInActivity.java
+++ app/src/main/java/com/competition/scriptkillingapp/view/activity/LogInActivity.java
@@ -2,9 +2,12 @@ package com.competition.scriptkillingapp.view.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,6 +31,14 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //设置顶部框透明
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         //找到控件
         mBtnLogin = findViewById(R.id.login_btn2);
--- app/src/main/java/com/competition/scriptkillingapp/view/fragment/MyFragment.java
+++ app/src/main/java/com/competition/scriptkillingapp/view/fragment/MyFragment.java
@@ -1,24 +1,44 @@
 package com.competition.scriptkillingapp.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.competition.scriptkillingapp.R;
+import com.competition.scriptkillingapp.view.activity.LogInActivity;
+import com.competition.scriptkillingapp.view.activity.MainActivity;
 
 public class MyFragment extends Fragment {
     private View view;
+
+    private TextView mtxtViewSignOut;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_my, container, false);
 
+        mtxtViewSignOut = view.findViewById(R.id.my_tvSignOut);
+
+        initListener();
+
         return view;
+    }
+
+    private void initListener() {
+        mtxtViewSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), LogInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
